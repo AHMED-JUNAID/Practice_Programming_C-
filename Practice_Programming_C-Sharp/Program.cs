d39b09d03d5ca964b72e4a521d3b4d31b3013ad6
AHMED-JUNAID <37082978+AHMED-JUNAID@users.noreply.github.com>
2018-03-18 16:38:00 +0500

    variables

--- Practice_Programming_C-Sharp/Program.cs
+++ Practice_Programming_C-Sharp/Program.cs
@@ -9,7 +9,23 @@ namespace Practice_Programming_C_Sharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            int num1 = 0;
+            int num2 = 0;
+
+            Console.Write("enter any number : ");
+            num1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("enter another number : ");
+            num2 = Convert.ToInt16(Console.ReadLine());
+            
+            //multiplication of two numbers
+            //Console.WriteLine("the result is {0}", num2*num1);
+ 
+            //division of two numbers
+            Console.WriteLine("the number {0} divides with {1} is equal to {2}", num1, num2, num1/num2);
+
+            Console.ReadKey();
+
         }
     }
 }
