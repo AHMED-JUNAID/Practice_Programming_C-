ff10d49d4213d1f7f6cc063f310364178844ed0d
AHMED-JUNAID <37082978+AHMED-JUNAID@users.noreply.github.com>
2018-03-18 15:47:17 +0500

    first commit 18/03/2018

---
+++ Practice_Programming_C-Sharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practice_Programming_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
